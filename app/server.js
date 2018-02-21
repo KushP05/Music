0b3ba863686c44e47c166f2727e71ea0821d407d
Kushal <kushal.kp80@gmail.com>
2018-02-21 12:32:59 +0530

    updated the port

--- app/server.js
+++ app/server.js
@@ -10,6 +10,8 @@ app.get('*', (req,res)=>{
   res.sendFile(path.resolve(__dirname,'../public/index.html'));
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 console.log('server started')
